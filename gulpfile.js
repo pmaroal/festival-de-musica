c3b437e0d08850b52afad880d5baee0c104dec14
PedroMaroal <pedro.maroal@gmail.com>
2024-10-21 18:31:47 +0100

    conect gulp and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-export function hola(done) {
-  console.log("Hola desde Gulpfile.js");
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
+import { src, dest } from "gulp";
+
+const sass = gulpSass(dartSass); // gulp-sass
+
+export function css(done) {
+  src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
 
   done();
 }
